6e53fd10686a26a67c45b0e6ba39f178b3ed8247
Daniil Ermolaev <blcklptn@gmail.com>
2022-12-15 23:50:52 +0500

    Imgui connected to glfw

---
+++ build/CMakeFiles/GLMap.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/daniilermolaev/Desktop/GLMap/src/imgui/imgui.cpp" "CMakeFiles/GLMap.dir/src/imgui/imgui.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/imgui/imgui.cpp.o.d"
+  "/Users/daniilermolaev/Desktop/GLMap/src/imgui/imgui_demo.cpp" "CMakeFiles/GLMap.dir/src/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/imgui/imgui_demo.cpp.o.d"
+  "/Users/daniilermolaev/Desktop/GLMap/src/imgui/imgui_draw.cpp" "CMakeFiles/GLMap.dir/src/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/imgui/imgui_draw.cpp.o.d"
+  "/Users/daniilermolaev/Desktop/GLMap/src/imgui/imgui_impl_glfw.cpp" "CMakeFiles/GLMap.dir/src/imgui/imgui_impl_glfw.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/imgui/imgui_impl_glfw.cpp.o.d"
+  "/Users/daniilermolaev/Desktop/GLMap/src/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/GLMap.dir/src/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/Users/daniilermolaev/Desktop/GLMap/src/imgui/imgui_tables.cpp" "CMakeFiles/GLMap.dir/src/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/imgui/imgui_tables.cpp.o.d"
+  "/Users/daniilermolaev/Desktop/GLMap/src/imgui/imgui_widgets.cpp" "CMakeFiles/GLMap.dir/src/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/imgui/imgui_widgets.cpp.o.d"
+  "/Users/daniilermolaev/Desktop/GLMap/src/main.cpp" "CMakeFiles/GLMap.dir/src/main.cpp.o" "gcc" "CMakeFiles/GLMap.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/daniilermolaev/Desktop/GLMap/build/external/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
